31f2d25e9b96e257cfffd0e6ef2826dc994e1dcf
redicule777 <msjadhav88@gmail.com>
2019-09-25 08:16:01 +0100

    Added variables in pre-requsites so that the branch can be forked and variables can be set based on the Environment it is being deployed on.

--- pre-requisites/main.tf
+++ pre-requisites/main.tf
@@ -13,6 +13,13 @@ provider "aws" {
 }
 
 
+# Declare Variable for Pre-requisite Dynamo DB Table Name
+
+variable "prerequisite_bucket_name" {
+  default = "tf-state-store-mj"
+  type    = "string"
+}
+
 # Create a bucket.
 # SET ACL to Private.
 # Enable Versioning.
@@ -21,7 +28,7 @@ provider "aws" {
 # Create Tags.
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tf-state-store-mj"
+  bucket = "${var.prerequisite_bucket_name}"
   acl    = "private"
   versioning {
     enabled = true
@@ -35,15 +42,21 @@ resource "aws_s3_bucket" "terraform_state" {
   }
   force_destroy = true
   tags = {
-    Name = "terraform_state_store_mj"
+    Name = "${var.prerequisite_bucket_name}"
   }
 }
 
 
+# Declare Variable for Pre-requisite Dynamo DB Table Name
+variable "prerequisite_dynamodb_name" {
+  default = "terraform_state_lock"
+  type    = "string"
+}
+
 # Create a dynamodb Table with the primary key called  LockID.
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-state-lock"
+  name         = "${var.prerequisite_dynamodb_name}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
---
+++ pre-requisites/terraform.tfvars
@@ -0,0 +1,4 @@
+# Declare Pre-requistes Variables here based on your Environment.
+#################################################################
+prerequisite_bucket_name   = "tf-state-store-mj"
+prerequisite_dynamodb_name = "terraform_state_lock"
